85588e04cc0cbed5ad03ec83e003b9e5040fa2be
Paul Colomiets <paul@colomiets.name>
2016-03-22 01:02:17 +0200

    Add asyncio + redis example

---
+++ examples/http_asyncio_redis.py
@@ -0,0 +1,31 @@
+import asyncio
+import aioredis
+
+from functools import partial
+
+from stator import http, loop, aio
+
+
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    redis = loop.run_until_complete(
+        aioredis.create_redis("/work/target/redis.sock", loop=loop))
+
+    @asyncio.coroutine
+    def dispatch(loop, req):
+        n = yield from redis.incr("hello-world-counter")
+        req.reply(
+            [200, u"OK"],
+            {u"Content-Type": b"text/html"},
+            "Hello page opened {} times".format(n).encode('utf-8'))
+
+    sock = http.Http('0.0.0.0', 3000)
+    aio.start(dispatch, loop=loop)
+
+    loop.run_forever()
+
+
+if __name__ == '__main__':
+    main()
