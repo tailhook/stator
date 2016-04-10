a8debb37a0f65947726dd28fdbcceb8a9e71b47e
Paul Colomiets <paul@colomiets.name>
2016-04-10 22:49:10 +0300

    Fix uwsgi-redis example

--- examples/uwsgi-redis.py
+++ examples/uwsgi-redis.py
@@ -7,4 +7,4 @@ rconn = redis.Redis(unix_socket_path="/work/target/redis.sock")
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
     n = rconn.incr("hello-world-counter")
-    return ["Hello page opened {} times".format(n)]
+    return ["Hello page opened {} times".format(n).encode('utf-8')]
